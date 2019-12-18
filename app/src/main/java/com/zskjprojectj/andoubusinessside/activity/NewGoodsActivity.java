c8a7705c7dbe3fb9134894c5f66dbb0e8e256cfa
LongQing <RoxasLong@163.com>
2019-12-18 10:20:18 +0800

    商城商家布局数据绑定完成

--- app/src/main/java/com/zskjprojectj/andoubusinessside/activity/NewGoodsActivity.java
+++ app/src/main/java/com/zskjprojectj/andoubusinessside/activity/NewGoodsActivity.java
@@ -1,6 +1,10 @@
 package com.zskjprojectj.andoubusinessside.activity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +19,13 @@ public class NewGoodsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_new_goods);
         ((TextView) findViewById(R.id.actionBarTitleTxt)).setText("商品上传");
         findViewById(R.id.backBtn).setOnClickListener(view -> finish());
+        LinearLayout specContainer = findViewById(R.id.specContainer);
+        findViewById(R.id.newSpecBtn).setOnClickListener(view -> {
+            View specView = LayoutInflater.from(NewGoodsActivity.this).inflate(R.layout.layout_spec_item, specContainer, false);
+            View deleteBtn = specView.findViewById(R.id.deleteBtn);
+            specContainer.addView(specView);
+            deleteBtn.setVisibility(View.VISIBLE);
+            deleteBtn.setOnClickListener(view1 -> ((ViewGroup) specView.getParent()).removeView(specView));
+        });
     }
 }
